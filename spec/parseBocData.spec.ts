6a0cb89492c6e186d29925709d3c34a485a926b7
brpeanut <bresheske@gmail.com>
2017-12-07 13:00:56 -0600

    added boc

---
+++ spec/parseBocData.spec.ts
@@ -0,0 +1,19 @@
+import { AchParser } from "../services/achParser";
+import * as path from 'path';
+import { AchFile } from "../objects/achFile";
+import { RecordBocEntryDetail } from "../objects/recordBocEntryDetail";
+
+describe("PPD File Parsing", () => {
+    let parser = new AchParser();
+
+    it("parses basic ppd file", async (done) => {
+        let result = await parser.parseAchFile(path.join(__dirname, 'sampleData/basicBocFile.ach'));
+        expect(result.fileHeader).toBeDefined();
+        expect(result.batches).not.toBeNull();
+        expect(result.batches.length).toBe(1);
+        let boc = result.batches[0].entries[0] as RecordBocEntryDetail;
+        expect(boc.amount).toBe(74.22);
+        done();
+    });
+
+});
