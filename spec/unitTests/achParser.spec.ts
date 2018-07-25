612ff209eec73019e5facd5b11d3d1970e83c60b
brandon <bresheske@gmail.com>
2018-07-25 12:10:55 -0500

    one more test for piped data.

--- spec/unitTests/achParser.spec.ts
+++ spec/unitTests/achParser.spec.ts
@@ -1,12 +1,24 @@
 import { AchParser } from "../../services/achParser";
 import { assert } from "chai";
 import { Logger } from "../../services/logger";
+import * as fs from "async-file";
+import { Duplex } from "stream";
+import { AssertData } from "../sampleData/assertData";
 
 describe("Unit AchParser Tests", () => {
     let parser = new AchParser();
 
     beforeEach(() => {
         Logger.init(true);
+    });
+
+    it("should correctly accept piped data", async () => {
+        let data = await fs.readFile('./spec/sampleData/basicPpdFile.ach');
+        let stream = new Duplex();
+        stream.push(data);
+        stream.push(null);
+        let res = await parser.parseAchFile(stream);
+        AssertData.assertBasicPpdFile(res);
     });
 
     it("should fail when file does not exist", async () => {
